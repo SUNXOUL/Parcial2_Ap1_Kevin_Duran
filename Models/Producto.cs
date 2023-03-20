f3c6aa06a9a01a1085202061079eeb68c6c69d98
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-03-20 18:19:31 -0400

    CProductoAgregado

---
+++ Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_Ap1_Kevin_Duran.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public double  Costo { get; set; }
+        public int Existencia { get; set; }
+
+        public Producto()
+        {
+            this.Descripcion = string.Empty;
+            this.Precio = 0;
+            this.Costo = 0;
+            this.Existencia = 0;
+        }
+
+    }
+}
